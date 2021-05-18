d8cc32879fcf98fc567abd8dccaf663d501525d3
dapda99 <43702825+dapda99@users.noreply.github.com>
2021-05-18 10:18:59 +0700

    day1

---
+++ day1/SmartHome.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class SmartHome {
+    public void say(String message) {
+      System.out.println(message);
+    }
+  }
+  
---
+++ day1/util/SmartCar.java
@@ -0,0 +1,7 @@
+package day1.util;
+
+public class SmartCar {
+  public void run() {
+    System.out.println("smart car is running");
+  }
+}
